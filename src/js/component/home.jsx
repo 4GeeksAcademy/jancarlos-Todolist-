6d0055215cba6e41fa90bba467024f2b4c80ea3d
JancarlosT <138736732+JancarlosT@users.noreply.github.com>
2023-11-08 01:21:31 +0000

    tarea

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const Home = () => {
 
-	const [inputValue, setInputValue] = useState("pedro")
+	const [inputValue, setInputValue] = useState("")
 	const [list, setList] = useState([])
 	const [newTask, setNewTask] = useState("")
 	const getList = async () => {
@@ -24,9 +24,9 @@ const Home = () => {
 					label: newTask,
 					done: false
 				}
-				setList([...list, obj])
+				setList((prevList) => [...prevList, obj])
 				const response = await fetch(`https://playground.4geeks.com/apis/fake/todos/user/${inputValue}`, {
-					body: JSON.stringify(list),
+					body: JSON.stringify([...list, obj]),
 					method: "PUT",
 					headers: {
 						'Content-Type': 'application/json'
@@ -36,8 +36,9 @@ const Home = () => {
 				if (!response.ok) {
 					throw new Error("the response of get list give false")
 				}
-				const data = await response.json()
-				console.log(data)
+				await response.json();
+				getList();
+
 			} else {
 				alert("para guadar tarea necesita escribir algo")
 			}
@@ -47,13 +48,16 @@ const Home = () => {
 			console.log(err)
 		}
 	}
-    const hamdleDelete = (label)=>{
-		const newTaskList=list.filter((ele)=>ele.label!==label);
-		updateList(newTaskList)
+	const hamdleDelete = async (label) => {
+		const newTaskList = list.filter((ele) => ele.label !== label);
+
+		setList(newTaskList);
+		await updateList(newTaskList)
 	}
+
 	const updateList = async (newList) => {
 		try {
-			
+
 			const response = await fetch(`https://playground.4geeks.com/apis/fake/todos/user/${inputValue}`, {
 				body: JSON.stringify(newList),
 				method: "PUT",
@@ -64,46 +68,46 @@ const Home = () => {
 			})
 			if (response.ok) {
 				setList(newList)
-				
+
 			}
 		}
 		catch (err) {
-		console.log(err)
+			console.log(err)
+		}
 	}
-}
 
-useEffect(() => {
-	getList()
-}, [inputValue])
-return (
-	<div className="text-center container">
-		<h3>buscar tu lista por nombre:</h3>
-		<input
-			type="text"
-			onChange={(e) => setInputValue(e.target.value)}
-		/>
-		<h3>agregar una nueva tarea:</h3>
-		<input type="text"
-			onChange={(e) => setNewTask(e.target.value)}
-		/>
-		<button onClick={handlerClick}>agregar tarea</button>
-		<ul className="list-group list-group-flush">
+	useEffect(() => {
+		getList()
+	}, [inputValue])
+	return (
+		<div className="text-center container">
+			<h3>buscar tu lista por nombre:</h3>
+			<input
+				type="text"
+				onChange={(e) => setInputValue(e.target.value)}
+			/>
+			<h3>agregar una nueva tarea:</h3>
+			<input type="text"
+				onChange={(e) => setNewTask(e.target.value)}
+			/>
+			<button onClick={handlerClick}>agregar tarea</button>
+			<ul className="list-group list-group-flush">
 
-			{
-				list.map((ele, index) => {
-					return <li className="list-group-item" key={index}>{ele.label}
-						<button 
-						className="btn btn-outline-danger" onClick={()=>hamdleDelete(ele.label)}>X</button>
-					</li>
-				})
-
-
-			}
+				{
+					list.map((ele, index) => {
+						return <li className="list-group-item" key={index}>{ele.label}
+							<button
+								className="btn btn-outline-danger" onClick={() => hamdleDelete(ele.label)}>x</button>
+						</li>
+					})
 
 
-		</ul>
-	</div>
-);
+				}
+
+
+			</ul>
+		</div>
+	);
 };
 
 export default Home;
